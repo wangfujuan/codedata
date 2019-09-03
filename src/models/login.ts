c3129e46a89b6832f618b4f1fc5744e1bc31e758
Fujuan Wang <wangfujuan@FujuandeiMac.local>
2019-09-03 16:01:06 +0800

    新的登录
    新的登录

---
+++ src/models/login.ts
@@ -0,0 +1,99 @@
+import { Reducer } from 'redux';
+import { routerRedux } from 'dva/router';
+import { Effect } from 'dva';
+import { stringify } from 'querystring';
+
+import { userLogin, getFakeCaptcha } from '@/services/user';
+import { setAuthority } from '@/utils/authority';
+import { getPageQuery } from '@/utils/utils';
+
+export interface StateType {
+  status?: 'ok' | 'error';
+  type?: string;
+  currentAuthority?: 'user' | 'guest' | 'admin';
+}
+
+export interface LoginModelType {
+  namespace: string;
+  state: StateType;
+  effects: {
+    login: Effect;
+    getCaptcha: Effect;
+    logout: Effect;
+  };
+  reducers: {
+    changeLoginStatus: Reducer<StateType>;
+  };
+}
+
+const Model: LoginModelType = {
+  namespace: 'login',
+
+  state: {
+    status: undefined,
+  },
+
+  effects: {
+    *login({ payload }, { call, put }) {
+      const response = yield call(userLogin, payload);
+      yield put({
+        type: 'changeLoginStatus',
+        payload: response,
+      });
+      // Login successfully
+      if (response.code === 200) {
+        const urlParams = new URL(window.location.href);
+        console.log(urlParams);
+        const params = getPageQuery();
+        console.log(params);
+        let { redirect } = params as { redirect: string };
+        if (redirect) {
+          const redirectUrlParams = new URL(redirect);
+          if (redirectUrlParams.origin === urlParams.origin) {
+            redirect = redirect.substr(urlParams.origin.length);
+            if (redirect.match(/^\/.*#/)) {
+              redirect = redirect.substr(redirect.indexOf('#') + 1);
+            }
+          } else {
+            window.location.href = redirect;
+            return;
+          }
+        }
+        yield put(routerRedux.replace(redirect || '/'));
+      }
+    },
+
+    *getCaptcha({ payload }, { call }) {
+      yield call(getFakeCaptcha, payload);
+    },
+    *logout(_, { put }) {
+      console.log('退出登录1');
+      // const { redirect } = getPageQuery();
+      console.log('退出登录2');
+      // redirect
+      if (window.location.pathname !== '/user/login') {
+        yield put(
+          routerRedux.replace({
+            pathname: '/user/login',
+            search: stringify({
+              redirect: window.location.href,
+            }),
+          }),
+        );
+      }
+    },
+  },
+
+  reducers: {
+    changeLoginStatus(state, { payload }) {
+      setAuthority(payload.currentAuthority);
+      return {
+        ...state,
+        status: payload.status,
+        type: payload.type,
+      };
+    },
+  },
+};
+
+export default Model;
